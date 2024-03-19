86ab215f592cad3be3f0c0e779a80cad655abf7c
gr <grim322@bk.ru>
2024-03-20 00:10:53 +0300

    import admin fix

--- foodgram-project-react-master-5/backend/users/admin.py
+++ foodgram-project-react-master-5/backend/users/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth import admin as auth_admin
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, admin as auth_admin
 
 from .models import Subscription
 
